e0af53f9762ca5ca1dccd6985561b294e7b0c5b9
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-04-07 21:19:52 -0300

    Adicionando componente Colaborador com estilos e estrutura

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css';
+const Colaborador = () => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/Kauapy.png' alt='Kauã De Carvalho'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Vinicius Neves</h4>
+                <h5>Instrutor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,4 +1,5 @@
 import "./Time.css";
+import Colaborador from "../Colaborador";
 const Time = (props) => {
   const css = { backgroundColor: props.corSecundaria };
   const tituloEstilo = { borderColor: props.corPrimaria };
@@ -6,6 +7,7 @@ const Time = (props) => {
   return (
     <section className="time" style={css}>
       <h3 style={tituloEstilo}>{props.nome}</h3>
+      <Colaborador></Colaborador>
     </section>
   );
 };
