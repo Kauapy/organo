1c5b1f8988e1cce71092c31bafc65f23427d4fa8
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-04-23 19:17:19 -0300

    Adiciona a biblioteca uuid e implementa geração de IDs únicos para os times no componente App

--- src/App.js
+++ src/App.js
@@ -2,39 +2,47 @@ import React, { useState } from "react";
 import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
+import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: "Programação",
       cor: "#D9F7E9",
       corPrimaria: "#57C278",
     },
     {
+      id: uuidv4(),
       nome: "Front-End",
       cor: "#E8F8FF",
       corPrimaria: "#82CFFA",
     },
     {
+      id: uuidv4(),
       nome: "Data Science",
       cor: "#F0F8E2",
       corPrimaria: "#A6D157",
     },
     {
+      id: uuidv4(),
       nome: "Devops",
       cor: "#FDE7E8",
       corPrimaria: "#E06B69",
     },
     {
+      id: uuidv4(),
       nome: "UX e Design",
       cor: "#FAE9F5",
       corPrimaria: "#DB6EBF",
     },
     {
+      id: uuidv4(),
       nome: "Mobile",
       cor: "#FFF5D9",
       corPrimaria: "#FFBA05",
     },
     {
+      id: uuidv4(),
       nome: "Inovação e Gestão",
       cor: "#FFEEDF",
       corPrimaria: "#FF8A29",
