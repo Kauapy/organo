d00e0ceb0b385828456819d0856d25d6ec8bd55a
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-04-23 18:19:38 -0300

    Adiciona a dependência hex-to-rgba e refatora componentes para utilizar nova lógica de cores

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,18 +1,19 @@
 import "./Time.css";
 import Colaborador from "../Colaborador";
+import hexToRgba from 'hex-to-rgba';
 
 const Time = (props) => {
-  const css = { backgroundColor: props.corSecundaria };
   const tituloEstilo = { borderColor: props.corPrimaria };
 
   return (
     props.colaboradores.length > 0 && (
-      <section className="time" style={css}>
+      <section className="time" style={{backgroundColor: hexToRgba(props.cor, '0.6')}}>
         <input
           onChange={(evento) => props.mudarCor(evento.target.value, props.nome)}
           value={props.corPrimaria}
           type="color"
           className="input-cor"
+          
         ></input>
         <h3 style={tituloEstilo}>{props.nome}</h3>
         <div className="colaboradores">
